bfe0ab2fbaa5788f1b9a7dd47882fbd379d9658b
RasulRustamli <rasular@code.edu.az>
2022-09-20 01:58:47 +0400

    Add project files.

---
+++ MVC_Tutorial/Data/MVC_TutorialContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Tutorial.Models;
+
+namespace MVC_Tutorial.Data
+{
+    public class MVC_TutorialContext : DbContext
+    {
+        public MVC_TutorialContext (DbContextOptions<MVC_TutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Tutorial.Models.Movie> Movie { get; set; } = default!;
+    }
+}
